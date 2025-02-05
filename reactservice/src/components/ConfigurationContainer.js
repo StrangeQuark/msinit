c6bb5096aa9567b6e131aaad5d9c5df62e1060c3
StrangeQuark <zacharyzirkle@hotmail.com>
2025-02-04 19:32:10 -0600

    Real time validation on Project name and Project group inputs

--- reactservice/src/components/ConfigurationContainer.js
+++ reactservice/src/components/ConfigurationContainer.js
@@ -2,6 +2,24 @@ import React from "react";
 import "./css/ConfigurationContainer.css";
 
 const ConfigurationContainer = ({projectUtil}) => {
+    const handleGroupInputChange = (e) => {
+        const newValue = e.target.value
+        const isValid = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*$/.test(newValue)
+      
+        if (isValid || newValue === '') {
+          projectUtil.setProjectGroup(newValue)
+        }
+      }
+
+      const handleNameInputChange = (e) => {
+        const newValue = e.target.value
+        const isValidFilename = /^[^<>:"/\\|?*]+$/.test(newValue); // Valid filename characters check
+      
+        if (isValidFilename || newValue === '') {
+          projectUtil.setProjectName(newValue)
+        }
+      }
+    
     return(
         <div id="configurationContainer" className="configurationContainer">
             <div></div>
@@ -10,10 +28,10 @@ const ConfigurationContainer = ({projectUtil}) => {
                 <h2>Metadata</h2>
 
                 <label>Project name:</label>
-                <input value={projectUtil.projectName} onChange={(e) => projectUtil.setProjectName(e.target.value)} />
+                <input value={projectUtil.projectName} onInput={handleNameInputChange} />
 
                 <label>Project group:</label>
-                <input value={projectUtil.projectGroup} onChange={(e) => projectUtil.setProjectGroup(e.target.value)}/>
+                <input value={projectUtil.projectGroup} onInput={handleGroupInputChange}/>
             </div>
         </div>
     )
