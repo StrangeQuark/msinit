fee63f3619ed115a2e5d35017f99e7561d9fa725
StrangeQuark <zacharyzirkle@hotmail.com>
2026-09-16 10:12:04 -0500

    Remove matrix workspace

--- test_scripts/matrix-workspace/emailservice-main/src/main/java/com/example/emailservice/email/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.example.emailservice.email;
-
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- test_scripts/matrix-workspace/vaultservice-main/src/main/java/com/example/vaultservice/serviceuser/ServiceUserRole.java
+++
@@ -1,9 +0,0 @@
-// Integration file: Auth
-
-package com.example.vaultservice.serviceuser;
-
-public enum ServiceUserRole {
-    OWNER,
-    MANAGER,
-    MAINTAINER
-}
